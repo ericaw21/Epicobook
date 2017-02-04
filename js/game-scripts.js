894d6793c212810b7fea4eebdf552addb9fefca1
Erica Wright <ericaw21@gmail.com>
2017-02-03 16:54:39 -0800

    update game scripts to tally clicks and points for two players

--- js/game-scripts.js
+++ js/game-scripts.js
@@ -6,32 +6,51 @@ function Game(numberOfAccounts) {
   this.flip = 0;
 }
 
+function Player(points) {
+  this.points = 0;
+  this.click = 0;
+}
+
+var twoClicks = [];
+
+Player.prototype.turnCompare = function() {
+  var currentGame;
+  if (twoClicks[0] === twoClicks[1]) {
+    this.points ++;
+    $(twoClicks[0]).attr("class").hide();
+    $(twoClicks[1]).attr("class").hide();
+    alert(this.points);
+  } else {
+    $(twoClicks[0]).attr("class").hide();
+    $(twoClicks[1]).attr("class").hide();
+    currentGame.turn ++;
+      alert(currentGame.turn);
+  }
+}
+
+Player.prototype.turn = function(turnClicks) {
+  if (twoClicks.length === 2) {
+    this.click ++;
+    alert(twoClicks);
+    alert(this.click);
+    this.turnCompare(twoClicks);
+    return twoClicks = [];
+  } else {
+    this.click ++;
+  }
+}
+
 Game.prototype.clearCards = function() {
   this.studentCards = [];
 }
+
 Game.prototype.addCard = function(accountItems) {
   this.studentCards.push(accountItems);
 }
-Game.prototype.calcFlip = function() {
-  if (this.flip === 0) {
-    this.flip = 1;
-  } else {
-    this.flip++;
-  }
-  return this.flip;
-}
 
-function comparison(flipClass1, flipClass2) {
-  if (flipClass1 !== flipClass2) {
-    this.turn = 1;
-    this.flip = 0;
-    return false;
-  } else if (flipClass1 === flipClass2) {
-    this.turn++;
-    this.flip = 0;
-    return true;
-  }
-}
+// Game.prototype.endGame = function() {
+//   if (this.turn === )
+// }
 
 function random(max) {
   return Math.floor(Math.random() * max);
@@ -73,9 +92,11 @@ $(document).ready(function() {
     event.preventDefault();
     $("div#game-board").empty();
     var userInput = $(".student-number").val();
-    var game = new Game(userInput);
-    var comparFlip1 = "", comparFlip2 = "";
-    game.clearCards();
+    var currentGame = new Game(userInput);
+    var playerOne = new Player(0);
+    var playerTwo = new Player(0);
+    // var comparFlip1 = "", comparFlip2 = "";
+    currentGame.clearCards();
 
     for (var index = 1; index <= userInput; index ++) {
       var tempRandomNumber = random(accounts.length);
@@ -83,42 +104,54 @@ $(document).ready(function() {
       var pictureDisplay = "<img src='" + accounts[tempRandomNumber].pictureURL + "' alt='student image' class='hidden'>";
       namePicNumAssociation.push(pictureDisplay);
       namePicNumAssociation.push(tempRandomNumber);
-      game.addCard(namePicNumAssociation);
+      currentGame.addCard(namePicNumAssociation);
       namePicNumAssociation = [];
       var nameDisplay = "<p class='hidden'>" + accounts[tempRandomNumber].firstName + "</p>";
       namePicNumAssociation.push(nameDisplay);
       namePicNumAssociation.push(tempRandomNumber);
-      game.addCard(namePicNumAssociation);
+      currentGame.addCard(namePicNumAssociation);
     }
-    // alert(game.studentCards);
-    shuffle(game.studentCards);
+    shuffle(currentGame.studentCards);
 
-    game.studentCards.forEach(function(cardItem) {
+    currentGame.studentCards.forEach(function(cardItem) {
       var itemToDisplay = cardItem[0];
       var classNumber = cardItem[1].toString();
       $("#game-board").append("<div>" + itemToDisplay + "</div>");
       $("div#game-board").children("div").last().addClass(classNumber);
 
 
-      $("div#game-board div").last().click(function() {
-        $(this).children("img, p").toggle();
-        if (!comparFlip1) {
-          comparFlip1 = $(this).last().attr("class");
-        } else if (!comparFlip2) {
-          comparFlip2 = $(this).last().attr("class");
-        }
-        game.calcFlip(comparFlip1, comparFlip2);
-
-        if (comparison(comparFlip1, comparFlip2)) {
-          comparFlip1 = "";
-          comparFlip2 = "";
-          alert("NICE!");
-        }
-      });
-
+      if (currentGame.turn % 2 === 0) {
+        $("div#game-board div").last().click(function() {
+          $(this).children("img, p").show();
+          twoClicks.push($(this).last().attr("class"));
+          currentGame.flip ++;
+          playerTwo.turn();
+        });
+      } else {
+        $("div#game-board div").last().click(function() {
+          $(this).children("img, p").show();
+          twoClicks.push($(this).last().attr("class"));
+          currentGame.flip ++;
+          playerOne.turn();
+          // if () {
+          //
+          // } else {
+          //
+          // }
+        });
+      }
 
     }); // end of forEach function
 
+    // if (currentGame.flip % 2 === 0) {
+    //
+    // } else {
+    //
+    // }
+
+
+
   }); // end of submit
+
 
 }); // end of $(document).ready
